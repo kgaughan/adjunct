084cb7c9cd9e83830475baa046e7843cb8e072de
Keith Gaughan <k@stereochro.me>
2013-03-16 05:00:32 +0000

    Rejig discovery to allow feed discovery to be built on top of it.

--- adjunct/discovery.py
+++ adjunct/discovery.py
@@ -10,7 +10,9 @@ import urlparse
 
 
 __all__ = (
+    'LinkExtractor',
     'fetch_links',
+    'fix_attributes',
 )
 
 
@@ -29,27 +31,43 @@ class LinkExtractor(HTMLParser.HTMLParser):
         self.collected = []
 
     def handle_starttag(self, tag, attrs):
+        tag = tag.lower()
         if tag == 'head':
             self.active = True
         elif self.active:
+            attrs = fix_attributes(attrs)
             if tag == 'link':
-                self.collected.append(dict(attrs))
+                self.append(attrs)
             elif tag == 'base' and 'href' in attrs:
                 self.base = urlparse.urljoin(self.base, attrs['href'])
 
     def handle_endtag(self, tag):
-        if tag == 'head':
+        if tag.lower() == 'head':
             self.active = False
             self.finished = True
 
-    @staticmethod
-    def extract(fh, base='.'):
+    def append(self, attrs):
+        """
+        Append the given set of attributes onto our list.
+
+        By separating this out, we can modify the behaviour in subclasses.
+        """
+        self.collected.append(attrs)
+
+    def fix_href(self, href):
+        """
+        Make the given href absolute to any previously discovered <base> tag.
+        """
+        return urlparse.urljoin(self.base, href)
+
+    @classmethod
+    def extract(cls, fh, base='.'):
         """
         Extract the link tags from header of a HTML document to be read from
         the file object, `fh`. The return value is a list of dicts where the
         values of each are the attributes of the link elements encountered.
         """
-        parser = LinkExtractor(base)
+        parser = cls(base)
         with contextlib.closing(parser):
             while not parser.finished:
                 chunk = fh.read(2048)
@@ -60,15 +78,28 @@ class LinkExtractor(HTMLParser.HTMLParser):
         # Canonicalise the URL paths.
         for link in parser.collected:
             if 'href' in link:
-                link['href'] = urlparse.urljoin(parser.base, link['href'])
+                link['href'] = parser.fix_href(link['href'])
 
         return parser.collected
 
 
-def fetch_links(url):
+def fix_attributes(attrs):
+    """
+    Normalise and clean up the attributes, and put them in a dict.
+    """
+    result = {}
+    for attr, value in attrs:
+        attr = attr.lower()
+        if attr in ('rel', 'type'):
+            value = value.lower()
+        result[attr] = value.strip()
+    return result
+
+
+def fetch_links(url, extractor=LinkExtractor):
     """
     Extract the <link> tags from the HTML document at the given URL.
     """
     fh = urllib2.urlopen(url)
     with contextlib.closing(fh):
-        return LinkExtractor.extract(fh, url)
+        return extractor.extract(fh, url)

090affd0c18407e14a9703efab0a2cdba15c5982
Keith Gaughan <k@stereochro.me>
2013-03-16 01:11:09 +0000

    The <base> element, that's a thing. Also, make all URLs absolute.

--- adjunct/discovery.py
+++ adjunct/discovery.py
@@ -6,6 +6,7 @@ Discovery via HTML <link> elements.
 import contextlib
 import HTMLParser
 import urllib2
+import urlparse
 
 
 __all__ = (
@@ -20,17 +21,21 @@ class LinkExtractor(HTMLParser.HTMLParser):
     header of a HTML document.
     """
 
-    def __init__(self):
+    def __init__(self, base):
         HTMLParser.HTMLParser.__init__(self)
         self.active = False
         self.finished = False
+        self.base = base
         self.collected = []
 
     def handle_starttag(self, tag, attrs):
         if tag == 'head':
             self.active = True
-        elif self.active and tag == 'link':
-            self.collected.append(dict(attrs))
+        elif self.active:
+            if tag == 'link':
+                self.collected.append(dict(attrs))
+            elif tag == 'base' and 'href' in attrs:
+                self.base = urlparse.urljoin(self.base, attrs['href'])
 
     def handle_endtag(self, tag):
         if tag == 'head':
@@ -38,19 +43,25 @@ class LinkExtractor(HTMLParser.HTMLParser):
             self.finished = True
 
     @staticmethod
-    def extract(fh):
+    def extract(fh, base='.'):
         """
         Extract the link tags from header of a HTML document to be read from
         the file object, `fh`. The return value is a list of dicts where the
         values of each are the attributes of the link elements encountered.
         """
-        parser = LinkExtractor()
+        parser = LinkExtractor(base)
         with contextlib.closing(parser):
             while not parser.finished:
                 chunk = fh.read(2048)
                 if chunk == '':
                     break
                 parser.feed(chunk)
+
+        # Canonicalise the URL paths.
+        for link in parser.collected:
+            if 'href' in link:
+                link['href'] = urlparse.urljoin(parser.base, link['href'])
+
         return parser.collected
 
 
@@ -60,4 +71,4 @@ def fetch_links(url):
     """
     fh = urllib2.urlopen(url)
     with contextlib.closing(fh):
-        return LinkExtractor.extract(fh)
+        return LinkExtractor.extract(fh, url)

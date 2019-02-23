dd78b10d77fae03d788cab8b9bd56948a28742d5
Keith Gaughan <k@stereochro.me>
2019-02-23 23:17:34 +0000

    Test suite for xmlutils.

---
+++ tests/test_xmlutils.py
@@ -0,0 +1,48 @@
+import io
+import unittest
+
+from adjunct.xmlutils import XMLBuilder
+
+
+class XMLBuilderTest(unittest.TestCase):
+
+    def test_basics(self):
+        xml = XMLBuilder()
+        self.assertIsNotNone(xml.buffer)
+        with xml.within("root", xmlns="tag:talideon.com,2013:test"):
+            xml += "Before"
+            with xml.within("leaf"):
+                xml += "Within"
+            xml += "After"
+            xml.tag("leaf", "Another")
+        self.assertEqual(
+            xml.as_string(),
+            """<?xml version="1.0" encoding="utf-8"?>
+<root xmlns="tag:talideon.com,2013:test">Before<leaf>Within</leaf>After<leaf>Another</leaf></root>""",
+        )
+        xml.close()
+        self.assertIsNone(xml.buffer)
+
+    def test_own_buffer(self):
+        buf = io.StringIO()
+        xml = XMLBuilder(buf)
+        self.assertIsNone(xml.buffer)
+        with xml.within("root", xmlns="myns"):
+            xml += "Body"
+        self.assertIsNone(xml.as_string())
+        self.assertEqual(
+            buf.getvalue(),
+            """<?xml version="1.0" encoding="utf-8"?>
+<root xmlns="myns">Body</root>""",
+        )
+        xml.close()
+
+    def test_attrs(self):
+        xml = XMLBuilder()
+        xml.root(xmlns="myns")
+        self.assertEqual(
+            xml.as_string(),
+            """<?xml version="1.0" encoding="utf-8"?>
+<root xmlns="myns"></root>""",
+        )
+        xml.close()

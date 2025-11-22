c5ea50b3c4ed3ed81aa2b7c0f7b5931ddf71480e
Keith Gaughan <k@stereochro.me>
2025-11-22 23:12:50 +0000

    Simplify io types even further

--- src/adjunct/opml.py
+++ src/adjunct/opml.py
@@ -5,7 +5,6 @@
 
 import datetime
 import email.utils
-import io
 import typing as t
 import xml.sax
 import xml.sax.handler
@@ -153,7 +152,7 @@ def parse_timestamp(ts: str) -> datetime.datetime | None:
     return datetime.datetime.fromtimestamp(email.utils.mktime_tz(tt), tz=datetime.UTC)
 
 
-def parse(fh: io.TextIOBase) -> Outline | None:
+def parse(fh: t.TextIO) -> Outline | None:
     """Parses an OPML file from the given file object.
 
     Args:
